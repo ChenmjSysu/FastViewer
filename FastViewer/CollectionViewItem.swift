9496b4dd3350c6a84b77362573cf0652f3e1f199
chenmingjin <30458072@qq.com>
2020-03-10 01:28:01 +0800

    [feature] 加载图像显示到collectionview的功能

---
+++ FastViewer/CollectionViewItem.swift
@@ -0,0 +1,36 @@
+//
+//  CollectionViewItem.swift
+//  FastViewer
+//
+//  Created by 陈铭津 on 2020/3/10.
+//  Copyright © 2020 chenmingjin. All rights reserved.
+//
+
+import Cocoa
+
+class CollectionViewItem: NSCollectionViewItem {
+
+    // 1 define imageFile property
+    var imageFile: ImageFile? {
+        didSet {
+            guard isViewLoaded else { return }
+            if let imageFile = imageFile {
+                imageView?.image = imageFile.thumbnail
+                textField?.stringValue = imageFile.fileName
+            } else {
+                imageView?.image = nil;
+                textField?.stringValue = "";
+            }
+        }
+    }
+    
+    // 2 change background of item's view
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+        view.wantsLayer = true;
+        view.layer?.backgroundColor = NSColor.lightGray.cgColor;
+    }
+    
+}
--- FastViewer/ViewController.swift
+++ FastViewer/ViewController.swift
@@ -10,14 +10,30 @@ import Cocoa
 
 class ViewController: NSViewController {
     
+    @IBOutlet weak var collectionView: NSCollectionView!
     let imageDirectoryLoader = ImageDirectoryLoader()
+    
+    private func configureCollectionView() {
+        // 1 create layout
+        let flowLayout = NSCollectionViewFlowLayout();
+        flowLayout.itemSize = NSSize(width: 160.0, height: 140.0);
+        flowLayout.sectionInset = NSEdgeInsets(top: 10.0, left: 20.0, bottom: 10.0, right: 20.0);
+        flowLayout.minimumInteritemSpacing = 20.0;
+        flowLayout.minimumLineSpacing = 20.0;
+        collectionView.collectionViewLayout = flowLayout;
+        // 2 For optimal performance, NSCollectionView is designed to be layer-backed
+        view.wantsLayer = true;
+        // 3 set background color
+        collectionView.layer?.backgroundColor = NSColor.black.cgColor;
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let initialFolderUrl = NSURL.fileURL(withPath: "/Users/chenmingjin/Desktop", isDirectory: true)
-        imageDirectoryLoader.loadDataForFolderWithUrl(folderURL: initialFolderUrl as NSURL)
+        let initialFolderUrl = NSURL.fileURL(withPath: "/Users/chenmingjin/Downloads", isDirectory: true);
+        imageDirectoryLoader.loadDataForFolderWithUrl(folderURL: initialFolderUrl as NSURL);
+        configureCollectionView();
     }
 
     override var representedObject: Any? {
@@ -47,8 +63,50 @@ class ViewController: NSViewController {
     }
     
     func loadDataForNewFolderWithUrl(folderURL: NSURL) {
-        imageDirectoryLoader.loadDataForFolderWithUrl(folderURL: folderURL)
+        // reload files
+        imageDirectoryLoader.loadDataForFolderWithUrl(folderURL: folderURL);
+        // redisplay files
+        collectionView.reloadData();
     }
-
 }
+
+extension ViewController : NSCollectionViewDataSource {
+    // 3 返回某个index的item
+    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        // 4
+        let item = collectionView.makeItem(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "CollectionViewItem"), for: indexPath as IndexPath)
+        guard let collectionViewItem = item as? CollectionViewItem else {return item}
+        
+        // 5
+        let imageFile = imageDirectoryLoader.imageFileForIndexPath(indexPath: indexPath as NSIndexPath)
+        collectionViewItem.imageFile = imageFile
+        return item
+    }
+    
+  
+  // 1 返回section个数
+  func numberOfSectionsInCollectionView(collectionView: NSCollectionView) -> Int {
+    return imageDirectoryLoader.numberOfSections
+  }
+  
+  // 2 返回特定section的item个数
+    func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+    return imageDirectoryLoader.numberOfItemsInSection(section: section)
+  }
+  
+  // 3 返回某个index的item
+  func collectionView(_collectionView: NSCollectionView, itemForRepresentedObjectAtIndexPath indexPath: NSIndexPath) -> NSCollectionViewItem {
+    
+    // 4
+    let item = collectionView.makeItem(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "CollectionViewItem"), for: indexPath as IndexPath)
+    guard let collectionViewItem = item as? CollectionViewItem else {return item}
+    
+    // 5
+    let imageFile = imageDirectoryLoader.imageFileForIndexPath(indexPath: indexPath)
+    collectionViewItem.imageFile = imageFile
+    return item
+  }
+  
+}
+
 
